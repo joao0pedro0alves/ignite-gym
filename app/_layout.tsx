af7eb290379d5cff287e93fc1e9d14f6502dae2c
João Alves <joao.alves@ecosis.com.br>
2023-06-26 14:25:40 -0300

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function RootLayout() {
+  return (
+    <View className="flex-1 items-center justify-center bg-zinc-800">
+      <Text className="font-bold text-green-500">IGNITE GYM</Text>
+    </View>
+  )
+}
